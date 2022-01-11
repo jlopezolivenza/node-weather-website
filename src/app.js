5929e2e230274c4084edc571d35b9c4581bdd875
Josu Lopez Olivenza <jlopezolivenza@gmail.com>
2022-01-11 15:44:25 +0100

    First commit

---
+++ src/app.js
@@ -0,0 +1,36 @@
+const express = require('express')
+
+const app = express()
+
+// app.com
+app.get('', (req, res)=>{
+    res.send('<h1>Weather</h1>')
+})
+
+// app.com/help
+app.get('/help', (req, res)=>{
+    res.send([{
+        name: 'Josu',
+        age: 27
+    },{
+        name: 'Alba',
+        age: 26
+    }])
+})
+
+// app.com/about
+app.get('/about', (req, res)=>{
+    res.send('<h1>About</h1>')
+})
+
+// app.com/weather
+app.get('/weather', (req, res)=>{
+    res.send({
+        forecast: 'It is snowing',
+        location: 'Arrasate'
+    })
+})
+
+app.listen(3000, () =>{
+    console.log('Server is up on port 3000.')
+})
