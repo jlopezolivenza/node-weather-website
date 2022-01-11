70cdf38d14abd20e110795eb801e7f289575c148
Josu Lopez Olivenza <jlopezolivenza@gmail.com>
2022-01-11 16:30:51 +0100

    added about and help .html files

--- src/app.js
+++ src/app.js
@@ -9,22 +9,6 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
 
-// app.com/help
-app.get('/help', (req, res)=>{
-    res.send([{
-        name: 'Josu',
-        age: 27
-    },{
-        name: 'Alba',
-        age: 26
-    }])
-})
-
-// app.com/about
-app.get('/about', (req, res)=>{
-    res.send('<h1>About</h1>')
-})
-
 // app.com/weather
 app.get('/weather', (req, res)=>{
     res.send({
