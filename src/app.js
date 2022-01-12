f8560e556315bc7a8cfa6f19287764b0e3620686
Josu Lopez Olivenza <jlopezolivenza@gmail.com>
2022-01-12 15:30:32 +0100

    all the views with dynamic content

--- src/app.js
+++ src/app.js
@@ -1,13 +1,33 @@
 const path = require('path')
 const express = require('express')
 
-console.log(__dirname)
-console.log(path.join(__dirname, '../public'))
-
 const app = express()
 const publicDirectoryPath = path.join(__dirname, '../public')
 
+app.set('view engine', 'hbs')
 app.use(express.static(publicDirectoryPath))
+
+// index
+app.get('', (req, res) =>{
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Josu Lopez'
+    })
+})
+// /about
+app.get('/about', (req, res)=>{
+    res.render('about', {
+        title: 'About Me',
+        name: 'Josu Lopez'
+    })
+})
+// /help
+app.get('/help', (req, res) =>{
+    res.render('help', {
+        message: 'Este mensaje de ayuda es nuevo'
+    })
+})
+
 
 // app.com/weather
 app.get('/weather', (req, res)=>{
