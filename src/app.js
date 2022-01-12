32c1a43d87a0fda6080ba801d65385af24d56405
Josu Lopez Olivenza <jlopezolivenza@gmail.com>
2022-01-12 17:12:47 +0100

    added 404 error code

--- src/app.js
+++ src/app.js
@@ -1,10 +1,20 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express()
-const publicDirectoryPath = path.join(__dirname, '../public')
 
+// Define paths for express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+// Setup handlebars engin and views location
 app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+// Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
 
 // index
@@ -14,6 +24,7 @@ app.get('', (req, res) =>{
         name: 'Josu Lopez'
     })
 })
+
 // /about
 app.get('/about', (req, res)=>{
     res.render('about', {
@@ -21,13 +32,15 @@ app.get('/about', (req, res)=>{
         name: 'Josu Lopez'
     })
 })
+
 // /help
 app.get('/help', (req, res) =>{
     res.render('help', {
-        message: 'Este mensaje de ayuda es nuevo'
+        message: 'Este mensaje de ayuda es nuevo',
+        title: 'Help',
+        name: 'Josu Lopez'
     })
 })
-
 
 // app.com/weather
 app.get('/weather', (req, res)=>{
@@ -37,6 +50,23 @@ app.get('/weather', (req, res)=>{
     })
 })
 
+// Article not found
+app.get('/help/*', (req, res)=>{
+    res.render('404', {
+        title: 'ERROR 404',
+        message: 'Article not found'
+    })
+})
+
+// 404 handler
+app.get('*',(req, res) =>{
+    res.render('404', {
+        title: 'ERROR 404',
+        message: 'Page not found'
+    })
+})
+
+// Put up the server listening in 3000 port
 app.listen(3000, () =>{
     console.log('Server is up on port 3000.')
 })
