4190030f7ab4c3edfd58b87c5ba8b8ba22097e44
Josu Lopez Olivenza <jlopezolivenza@gmail.com>
2022-01-14 13:48:24 +0100

    Final version course weather-server-app

--- src/app.js
+++ src/app.js
@@ -51,16 +51,16 @@ app.get('/weather', (req, res)=>{
             error: 'You must provide an address'
         })
     }
-    geocode (req.query.address, (error, data)=>{
+    geocode (req.query.address, (error, {latitude, longitude, location} = {})=>{
         if (error){
             return res.send({error})
         }
-        forecast(data.latitude, data.longitude, (error, forecastData) => {
+        forecast(latitude, longitude, (error, forecastData) => {
             if (error){
                 return res.send({error})
             }
             res.send({
-                location: data.location,
+                location: location,
                 forecast: forecastData,
                 address: req.query.address
             })
