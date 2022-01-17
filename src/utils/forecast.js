9fd9742727a1e8caed140bb1111abd6a9117c511
Josu Lopez Olivenza <jlopezolivenza@gmail.com>
2022-01-17 15:32:04 +0100

    added new data. Humidity

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,5 +1,9 @@
 const request = require('postman-request')
 
+
+
+
+
 const forecast = (latitude, longitude, callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=57ab958cca1092f56509052f2a6b350b&query=' + latitude + ',' + longitude +'&units=f'
     request({url: url, json: true}, (error, response)=>{
@@ -10,10 +14,9 @@ const forecast = (latitude, longitude, callback) => {
             callback('Unable to search the location. Please try with other place.', undefined)
         }
         else{
-            callback(undefined, /*{
-                location: response.body.location.name
-            }*/
-            response.body.current.weather_descriptions[0] + ". The temperature is " + response.body.current.temperature + " degrees out and it feels like " + response.body.current.feelslike + " degrees.")
+            callback(undefined, 
+            response.body.current.weather_descriptions[0] + ". The temperature is " + response.body.current.temperature + " degrees out and it feels like " + response.body.current.feelslike + " degrees."+
+            "The humidity is " + response.body.current.humidity )
         }
     })
 }
